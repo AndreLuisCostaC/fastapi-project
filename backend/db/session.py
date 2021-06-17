39cf04fd3cfef06179ab77b8bf4c0f0134ba3ef7
Andre Luis Costa Carvalho <andreluiscostacarvalho@gmail.com>
2021-06-17 15:19:14 -0400

    section 2 code

--- backend/db/session.py
+++ backend/db/session.py
@@ -2,11 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
+from typing import Generator
 
 SQKALCHEMY_DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(SQKALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 # db_name: db_course
 # user: andre
